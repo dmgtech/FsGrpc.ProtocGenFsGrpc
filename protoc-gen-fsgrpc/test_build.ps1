8e52b57be72b44935836436cc97bb9fe06cd0be0
Michael Ciccotti <mciccotti@divisionsinc.com>
2023-04-26 09:34:25 -0500

    remove transitive FsGrpc.Tools dependency

--- protoc-gen-fsgrpc/test_build.ps1
+++ protoc-gen-fsgrpc/test_build.ps1
@@ -4,4 +4,4 @@ dotnet build -r osx-x64 -p:TargetFramework=net6.0 --no-self-contained -c=release
 dotnet build -r win-x86 -p:TargetFramework=net6.0 --no-self-contained -c=release
 dotnet build -r win-x64 -p:TargetFramework=net6.0 --no-self-contained -c=release
 
-dotnet pack -p:PROTOCGENFSGRPC_VERSION=1.0.3 --configuration=Release -o=artifacts
+dotnet pack -p:PROTOCGENFSGRPC_VERSION=0.5.4-prerelease --configuration=Release -o=artifacts
