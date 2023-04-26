6d2126994790024067cf3f2a6ea34fa02816ff47
Michael Ciccotti <mciccotti@divisionsinc.com>
2023-04-26 10:36:40 -0500

    publish with multiple runtime identifiers

--- protoc-gen-fsgrpc/test_build.ps1
+++ protoc-gen-fsgrpc/test_build.ps1
@@ -1,7 +1,10 @@
-dotnet build -r linux-arm64 -p:TargetFramework=net6.0 --no-self-contained -c=release
-dotnet build -r linux-x64 -p:TargetFramework=net6.0 --no-self-contained -c=release
-dotnet build -r osx-x64 -p:TargetFramework=net6.0 --no-self-contained -c=release
-dotnet build -r win-x86 -p:TargetFramework=net6.0 --no-self-contained -c=release
-dotnet build -r win-x64 -p:TargetFramework=net6.0 --no-self-contained -c=release
+dotnet restore
 
-dotnet pack -p:PROTOCGENFSGRPC_VERSION=0.5.4-prerelease --configuration=Release -o=artifacts
+dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r linux-arm64 
+dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r linux-x64
+dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r osx-x64
+dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r osx-arm64
+dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r win-x86
+dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r win-x64
+
+dotnet pack -p:PROTOCGENFSGRPC_VERSION=0.5.4-prerelease --configuration=Release -o=artifacts --no-restore --no-build
