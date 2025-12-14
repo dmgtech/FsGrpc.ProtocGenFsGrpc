0c92ca16a20434522894ccdf9aad94246635b015
Gregory Allen <gregory.allen@divisionsinc.com>
2025-12-14 02:19:50 -0800

    update versions

--- protoc-gen-fsgrpc/test_build.ps1
+++ protoc-gen-fsgrpc/test_build.ps1
@@ -1,10 +1,10 @@
 dotnet restore
 
-dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r linux-arm64 
-dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r linux-x64
-dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r osx-x64
-dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r osx-arm64
-dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r win-x86
-dotnet build -p:TargetFramework=net6.0 --no-self-contained --configuration Release --no-restore -r win-x64
+dotnet build -p:TargetFramework=net8.0 --no-self-contained --configuration Release --no-restore -r linux-arm64 
+dotnet build -p:TargetFramework=net8.0 --no-self-contained --configuration Release --no-restore -r linux-x64
+dotnet build -p:TargetFramework=net8.0 --no-self-contained --configuration Release --no-restore -r osx-x64
+dotnet build -p:TargetFramework=net8.0 --no-self-contained --configuration Release --no-restore -r osx-arm64
+dotnet build -p:TargetFramework=net8.0 --no-self-contained --configuration Release --no-restore -r win-x86
+dotnet build -p:TargetFramework=net8.0 --no-self-contained --configuration Release --no-restore -r win-x64
 
 dotnet pack -p:PROTOCGENFSGRPC_VERSION=0.5.4-prerelease --configuration=Release -o=artifacts --no-restore --no-build
